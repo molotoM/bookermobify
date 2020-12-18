19dfa77fcaf62b27fa44c91fb54d241e805c4d3b
molotoM <molotomarkhams@gmail.com>
2020-12-18 10:11:17 +0200

    tine out added

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const port = process.env.PORT ||3001;
 const server = http.createServer(app);
 
 server.listen(port);
+server.setTimeout = 5000;
 
 console.log("Connected successfully on port: ",port);
